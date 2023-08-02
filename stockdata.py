714801f0461e1eae2b71fa9837111e29b2497481
BuildTools <unconfigured@null.spigotmc.org>
2023-08-01 22:52:25 -0500

    Dockerfile and cleaned up data collection

--- stockdata.py
+++ stockdata.py
@@ -1,7 +1,8 @@
 import yfinance as yf
 from pandas_datareader import data as pdr
+import pandas as pd
 from yahoo_fin import stock_info
-import time
+import os
 
 
 yf.pdr_override()
@@ -11,8 +12,8 @@ def get_news(ticker):
         message = ''
         stock = yf.Ticker(ticker)
         articles = stock.news
-        uuids_txt = open("tickers.txt", 'a')
-        uuids_values = open("tickers.txt", 'r').read().splitlines()
+        uuids_txt = open("discord/tickers.txt", 'a')
+        uuids_values = open("discord/tickers.txt", 'r').read().splitlines()
         titles = []
         links = []
         for article in articles:
@@ -28,43 +29,43 @@ def get_news(ticker):
             
             message += ticker + ": \n" + description + "\n"
         return message
-'''
-def get_data():
-    QQQ = yf.Ticker("QQQ")
-    TQQQ = yf.Ticker("TQQQ")
-    SQQQ = yf.Ticker("SQQQ")
 
-    print("--------- QQQ ---------")
-    print(QQQ.info)
-    print()
 
-    print("--------- TQQQ ---------")
-    print(TQQQ.info)
-    print()
+def download_data(ticker, period):
 
-    print("--------- SQQQ ---------")
-    print(SQQQ.info)
-    print()
-
-def get_news():
-    NASDAQ = yf.Ticker("Qr7H24986tgf")
-    for article in NASDAQ.news:
-        for link in article:
-            print(link)
-
-def download_data():
-    data = pdr.get_data_yahoo("<ticker>", start="YYYY-MM-DD", end="YYYY-MM-DD")
-    data.to_csv("output.csv")
+    # Download data for the given ticker
+    data = yf.download(tickers=ticker, 
+                       period=period, 
+                       interval="1m", 
+                       prepost = True,
+                       auto_adjust = False,
+                       repair = True)
     
-def get_live_data():
-    while True:
-        price = stock_info.get_live_price("TQQQ")
-        print(price)
-        time.sleep(5)
+    return data
+
+def update_csv(data, ticker):
+
+    path = "data/" + ticker + ".csv"
+
+    # If the CSV file already exists, read the existing data
+    if os.path.exists(path):
+        existing_data = pd.read_csv(path, index_col=0, parse_dates=True)
+        
+        # Append the new data to the existing data
+        combined_data = pd.concat([existing_data, data])
+    else:
+        combined_data = data
+    
+    # Remove duplicate rows, if any
+    combined_data = combined_data[~combined_data.index.duplicated(keep='first')]
+    
+    # Save the combined data to the CSV file
+    combined_data.to_csv(path)
+
+def download_data_and_update_csv(ticker, period):
+    data = download_data(ticker, period)
+    update_csv(data, ticker)
 
 
-
-
-'''
 if __name__ == "__main__":
     pass
