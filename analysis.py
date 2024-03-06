08dc93533d4eff2d45173b7d178403055e29c5d8
BuildTools <unconfigured@null.spigotmc.org>
2024-03-06 17:53:23 -0600

    begin revamping charts with mplfinance - MACD

--- analysis.py
+++ analysis.py
@@ -9,9 +9,12 @@ import pandas_ta as ta
 import pandas_ta.momentum as tamomentum
 import pandas_ta.volume as tavolume
 import pandas_ta.trend as tatrend
+import mplfinance as mpf
+import stockdata as sd
+import yfinance as yf
+
 
 # Plotting Technical Indicators
-
 def plot_rsi(data, ticker):
 
     close = data[['Date', 'Close']].copy().set_index('Date')
@@ -52,44 +55,45 @@ def plot_rsi(data, ticker):
     plt.close()
 
 def plot_obv(data, ticker):
+    data = data.set_index('Date')
     # Create two charts on the same figure.
-        ax1 = plt.subplot2grid((10,1), (0,0), rowspan = 4, colspan = 1)
-        ax2 = plt.subplot2grid((10,1), (5,0), rowspan = 4, colspan = 1, sharex=ax1)
+    ax1 = plt.subplot2grid((10,1), (0,0), rowspan = 4, colspan = 1)
+    ax2 = plt.subplot2grid((10,1), (5,0), rowspan = 4, colspan = 1, sharex=ax1)
 
-        period = 60
+    period = 60
 
-        obv_curr = data['OBV'].values[-1]
-        obv_old = data['OBV'].values[-period]
+    obv_curr = data['OBV'].values[-1]
+    obv_old = data['OBV'].values[-period]
 
-        close_curr = data['Close'].values[-1]
-        close_old = data['Close'].values[-period]
+    close_curr = data['Close'].values[-1]
+    close_old = data['Close'].values[-period]
 
-        obv_slope = (obv_curr - obv_old) / period
-        close_slope = (close_curr - close_old) / period
+    obv_slope = (obv_curr - obv_old) / period
+    close_slope = (close_curr - close_old) / period
 
 
-        xstart = len(data['Close']) - period
+    xstart = len(data['Close']) - period
 
-        # First chart:
-        # Plot the closing price on the first chart
-        ax1.plot(data['Close'], linewidth=2)
-        ax1.set_title(ticker.upper() + ' Close Price')
+    # First chart:
+    # Plot the closing price on the first chart
+    ax1.plot(data['Close'], linewidth=2)
+    ax1.set_title(ticker.upper() + ' Close Price')
 
-        # Create line indicating start of analysis period
-        ax1.axvline(x = xstart, linestyle = '--', color = 'red')
-        ax1.axline(xy1 = (xstart, close_old), slope  = close_slope, color = 'red')
+    # Create line indicating start of analysis period
+    ax1.axvline(x = xstart, linestyle = '--', color = 'red')
+    ax1.axline(xy1 = (xstart, close_old), slope  = close_slope, color = 'red')
 
-        # Second chart
-        # Plot the OBV
-        ax2.set_title('On-Balance Volume (' + ticker + ')')
-        ax2.plot(data['OBV'], color='purple', linewidth=1)
+    # Second chart
+    # Plot the OBV
+    ax2.set_title('On-Balance Volume (' + ticker + ')')
+    ax2.plot(data['OBV'], color='purple', linewidth=1)
 
-        # Create line indicating start of analysis period
-        ax2.axvline(x = xstart, linestyle = '--', color = 'red')
-        ax2.axline(xy1 = (xstart, obv_old), slope  = obv_slope, color = 'red')
+    # Create line indicating start of analysis period
+    ax2.axvline(x = xstart, linestyle = '--', color = 'red')
+    ax2.axline(xy1 = (xstart, obv_old), slope  = obv_slope, color = 'red')
 
-        plt.savefig("data/plots/" + ticker + "/" + ticker + "_OBV.png", dpi=1000)
-        plt.close()
+    plt.savefig("data/plots/" + ticker + "/" + ticker + "_OBV.png", dpi=1000)
+    plt.close()
 
 def plot_adi(data, ticker):
     # Create two charts on the same figure.
@@ -178,28 +182,92 @@ def plot_aroon(data, ticker):
         plt.savefig("data/plots/" + ticker + "/" + ticker + "_AROON.png", dpi=1000)
         plt.close()
 
-def plot_macd(data, ticker):
-    # Create two charts on the same figure.
-        ax1 = plt.subplot2grid((10,1), (0,0), rowspan = 4, colspan = 1)
-        ax2 = plt.subplot2grid((10,1), (5,0), rowspan = 4, colspan = 1, sharex=ax1)
-
-        # First chart:
-        # Plot the closing price on the first chart
-        ax1.plot(data['Close'], linewidth=2)
-        ax1.set_title(ticker.upper() + ' Close Price')
-
-        # Second chart
-        # Plot the OBV
-        ax2.set_title('Moving Average Convergence/Divergence (' + ticker + ')')
-        ax2.plot(data['MACD'], color='green', linewidth=1, label = 'MACD')
-        ax2.plot(data['MACD_SIGNAL'], color = 'orange', linewidth=1, label = 'MACD_SIGNAL')
-        ax2.legend()
+#Testing with pandas plot
         
-        # Plot zero-line
-        ax2.axhline(0, linestyle='--', linewidth=1.5, color='blue')
+def plot_macd(data, ticker):
 
-        plt.savefig("data/plots/" + ticker + "/" + ticker + "_MACD.png", dpi=1000)
-        plt.close()
+    def signal_buy(macd,macd_signal,close):
+        signal = []
+        previous_signal = -1
+        previous_macd = 0
+        for date, value in macd.items():
+            if value > macd_signal[date] and previous_macd < previous_signal:
+                signal.append(macd[date]*0.99999999999)
+            else:
+                signal.append(np.nan)
+            previous_macd = value
+            previous_signal =  macd_signal[date]
+        return signal
+
+    def signal_sell(macd,macd_signal,close):
+        signal = []
+        previous_signal = -1
+        previous_macd = 0
+        for date, value in macd.items():
+            if value < macd_signal[date] and previous_macd > previous_signal:
+                signal.append(macd[date]*0.99)
+            else:
+                signal.append(np.nan)
+            previous_macd = value
+            previous_signal =  macd_signal[date]
+        return signal
+
+
+    save      = dict(fname='data/plots/{}/{}_MACD.png'.format(ticker, ticker),dpi=500,pad_inches=0.25)
+    data      = get_macd(data)
+    exp12     = data['Close'].ewm(span=12, adjust=False).mean()
+    exp26     = data['Close'].ewm(span=26, adjust=False).mean()
+    macd      = data['MACD']
+    signal    = data['MACD_SIGNAL']
+    histogram = data['MACD_HISTOGRAM']
+    buy_signal = signal_buy(macd, signal, data['Close'])
+    sell_signal = signal_sell(macd, signal, data['Close'])
+
+    fb_green = dict(y1=macd.values,y2=signal.values,where=signal<macd,color="#93c47d",alpha=0.6,interpolate=True)
+    fb_red   = dict(y1=macd.values,y2=signal.values,where=signal>macd,color="#e06666",alpha=0.6,interpolate=True)
+    fb_green['panel'] = 1
+    fb_red['panel'] = 1
+    fb       = [fb_green,fb_red]
+
+    apds = [mpf.make_addplot(exp12,color='lime'),
+        mpf.make_addplot(exp26,color='c'),
+        mpf.make_addplot(buy_signal,panel=1, color='b', type='scatter'),
+        mpf.make_addplot(sell_signal,panel=1,color='orange',type='scatter'),
+        mpf.make_addplot(macd,panel=1,color='green',secondary_y=False),
+        mpf.make_addplot(signal,panel=1,color='yellow',secondary_y=False),#,fill_between=fb),
+        mpf.make_addplot(histogram,type='bar',width=0.7,panel=1,alpha=1,color='dimgray', secondary_y=True)
+        ]
+
+    #s = mpf.make_mpf_style(base_mpf_style='classic',rc={'figure.facecolor':'lightgray'})
+
+    mpf.plot(data,type='candle',ylabel='Close Price',addplot=apds,figscale=1.6,figratio=(6,5),title='\n\n{} MACD'.format(ticker),
+            style='default',panel_ratios=(1,1),fill_between=fb, savefig=save)#,show_nontrading=True)
+    
+def analyze_macd(data, ticker):
+    data = get_macd(data)
+    signal = ''
+    macd = data['MACD'].values[-1]
+    macd_signal = data['MACD_SIGNAL'].values[-1]
+
+    with open("data/analysis/{}/MACD.txt".format(ticker),'w') as macd_analysis: 
+        if (macd > 0 and macd > macd_signal):
+            signal = "BUY"
+            analysis = "MACD: **{}** - The MACD value is above 0 ({:,.2f}) and greater than the MACD signal line ({:,.2f}), indicating an uptrend".format(signal, macd, macd_signal)
+            macd_analysis.write(analysis)
+        elif (macd > 0 and macd <= macd_signal):
+            signal = "NEUTRAL"
+            analysis = "MACD: **{}** - The MACD value is above 0 ({:,.2f}) but less than the MACD signal line ({:,.2f}). Wait until it crosses the signal line to buy, or consider selling .".format(signal, macd, macd_signal)
+            macd_analysis.write(analysis)
+        elif (macd <= 0):
+            signal = "SELL"
+            analysis = "MACD: **{}** - The MACD value is below 0 ({:,.2f}), indicating a downtrend".format(signal, macd)
+            macd_analysis.write(analysis)
+
+def get_macd(data):
+    # Run Moving Average Convergence/Divergence (MACD) analysis 
+    macd = ta.macd(data['Close'])
+    data['MACD'], data['MACD_HISTOGRAM'], data['MACD_SIGNAL'] = macd['MACD_12_26_9'], macd['MACDh_12_26_9'], macd['MACDs_12_26_9'] 
+    return data
 
 def plot_stoch(data, ticker):
     # Create two charts on the same figure.
@@ -226,6 +294,9 @@ def plot_stoch(data, ticker):
 
         plt.savefig("data/plots/" + ticker + "/" + ticker + "_STOCH.png", dpi=1000)
         plt.close()
+    
+def format_plot(plot):
+    pass
 
 def generate_charts(data, ticker):
     
@@ -234,13 +305,13 @@ def generate_charts(data, ticker):
 
     # Generate technical indicator charts
 
-    plot_obv(data, ticker)
-    plot_adi(data, ticker)
-    plot_adx(data, ticker)
-    plot_aroon(data, ticker)
+    #plot_obv(data, ticker)
+    #plot_adi(data, ticker)
+    #plot_adx(data, ticker)
+    #plot_aroon(data, ticker)
     plot_macd(data, ticker)
-    plot_rsi(data, ticker)
-    plot_stoch(data, ticker)
+    #plot_rsi(data, ticker)
+    #plot_stoch(data, ticker)
     
 # Running analysis on techincal indicators to generate buy/sell signals
 
@@ -328,25 +399,6 @@ def analyze_aroon(data, ticker):
     analysis = ""
     return analysis
 
-def analyze_macd(data, ticker):
-    signal = ''
-    macd = data['MACD'].values[-1]
-    macd_signal = data['MACD_SIGNAL'].values[-1]
-
-    with open("data/analysis/{}/MACD.txt".format(ticker),'w') as macd_analysis: 
-        if (macd > 0 and macd > macd_signal):
-            signal = "BUY"
-            analysis = "MACD: **{}** - The MACD value is above 0 ({:,.2f}) and greater than the MACD signal line ({:,.2f}), indicating an uptrend".format(signal, macd, macd_signal)
-            macd_analysis.write(analysis)
-        elif (macd > 0 and macd <= macd_signal):
-            signal = "NEUTRAL"
-            analysis = "MACD: **{}** - The MACD value is above 0 ({:,.2f}) but less than the MACD signal line ({:,.2f}). Wait until it crosses the signal line to buy, or consider selling .".format(signal, macd, macd_signal)
-            macd_analysis.write(analysis)
-        elif (macd <= 0):
-            signal = "SELL"
-            analysis = "MACD: **{}** - The MACD value is below 0 ({:,.2f}), indicating a downtrend".format(signal, macd)
-            macd_analysis.write(analysis)
-
 def analyze_rsi(data, ticker):
     analysis = ''
     signal = ''
@@ -395,57 +447,68 @@ def generate_analysis(data, ticker):
     if not (os.path.isdir("data/analysis/" + ticker)):
         os.makedirs("data/analysis/" + ticker)
 
-    analyze_obv(data, ticker)
-    analyze_adi(data, ticker)
-    analyze_adx(data, ticker)
-    analyze_aroon(data, ticker)
+    #analyze_obv(data, ticker)
+    #analyze_adi(data, ticker)
+    #analyze_adx(data, ticker)
+    #analyze_aroon(data, ticker)
     analyze_macd(data, ticker)
-    analyze_rsi(data, ticker)
-    analyze_stoch(data, ticker)
-
-def generate_indicators(ticker):
-        
-    # Load the data into a dataframe
-    data = sd.download_data(ticker=ticker, period="1y", interval="1d")
-
+    #analyze_rsi(data, ticker)
+    #analyze_stoch(data, ticker)
+    
+def get_obv(data):
     # Run On-Balance Volume (OBV) analysis 
     data ['OBV'] = ta.obv(data['Close'], data['Volume'])
-    
+    return data
+
+def get_adi(data):
     # Run Accumulation/Distribution Index (ADI) analysis 
     data['ADI'] = tavolume.ad(data['High'], data['Low'], data['Close'], data['Volume'])
-    
+    return data
+
+def get_adx(data):
     # Run Average Directional Index (ADX) analysis  
     adx = tatrend.adx(data['High'], data['Low'], data['Close'])
     data['ADX'], data['ADX_DI+'], data['ADX_DI-'] = adx['ADX_14'], adx['DMP_14'], adx['DMN_14'] 
-    
+    return data
+
+def get_aroon(data):
     # Run Aroon Oscillator (AROON) analysis 
     aroon = tatrend.aroon(data['High'], data ['Low'])
     data['AROON_DOWN'], data['AROON_INDICATOR'], data['AROON_UP'] = aroon['AROOND_14'], aroon['AROONOSC_14'], aroon['AROONU_14'] 
-    
-    # Run Moving Average Convergence/Divergence (MACD) analysis 
-    macd = ta.macd(data['Close'])
-    data['MACD'], data['MACD_HISTOGRAM'], data['MACD_SIGNAL'] = macd['MACD_12_26_9'], macd['MACDh_12_26_9'], macd['MACDs_12_26_9'] 
-    
+    return data
+
+def get_stoch(data):
     # Run Stochastic Oscillator (STOCH) analysis 
     stoch = ta.stoch(close = data['Close'], high = data['High'], low = data['Low'])
     data['STOCH'], data['STOCH_SIGNAL']= stoch['STOCHk_14_3_3'], stoch['STOCHd_14_3_3'] 
-    
+    return data
+
+def get_rsi(data):
     # Run Relative Stength Index (RSI) analysis
-    data ['RSI'] = ta.rsi(data['Close'])
-        
-    sd.update_csv(data, ticker)
+    data['RSI'] = ta.rsi(data['Close'])
+    return data
 
 def run_analysis(tickers=sd.get_tickers()):
     for ticker in tickers:
-        generate_indicators(ticker)
+        sd.download_data_and_update_csv(ticker=ticker, period="1y", interval="1d")
+        #generate_indicators(ticker)
         data = sd.fetch_data(ticker)
         generate_charts(data, ticker)
         generate_analysis(data, ticker)
 
 
+def test():
+    # Testing mplfinance plot styles:
+
+    styles = mpf.available_styles()
+    data = sd.fetch_data('MSFT')
+    data = get_macd(data)
+    for style in styles:
+        mpf.plot(data, style=style, volume=True, title=style)
+
 if __name__ == '__main__':  
+    #test()
     pass
-    
 
 
 
