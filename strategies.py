6576054abe525592ba7267503dd99ca33b198ced
John Wolf <johnmwolf34@gmail.com>
2024-06-07 08:25:40 -0500

    More work on backtesting

--- strategies.py
+++ strategies.py
@@ -25,29 +25,24 @@ class SMA_10_50_Strategy(ta.Strategy):
         return an.signal_sma(data['Close'], 10, 50)
     
 # Strategies
-class SMA_10_50_ADX_Strategy(ta.Strategy, backtesting.Strategy):
+class SMA_10_50_ADX_Strategy(backtesting.Strategy):
 
-    def __init__(self,
-             name = "SMA 10/50 & ADX",
-             short_name = "SMA_10_50_ADX",
-             ta = [{"kind":"sma", "length":10}, {"kind":"sma", "length":50}, {'kind':'adx'}],
-             indicators = ['sma_10_50', 'adx']
-             ):
+    def __init__(self):
         
-        self.name =  name
-        self.short_name = short_name
-        self.ta = ta
-        self.indicators = indicators
-        backtesting.Strategy.init(self)
+        self.name =  "SMA 10/50 & ADX"
+        self.short_name = "SMA_10_50_ADX"
+        self.ta = [{"kind":"sma", "length":10}, {"kind":"sma", "length":50}, {'kind':'adx'}],
+        self.indicators = ['sma_10_50', 'adx']
+        
 
     def init(self):
-        pass
+        super().init()
 
     def signals(self, data):
         return an.signal_sma(data['Close'], 10, 50) & an.signal_adx(close=data['Close'], highs = data['High'], lows= data['Low'])
 
     def next(self):
-        super().next()
+        #super().next()
         signal = self.signals(data)[-1]
         if signal:
             self.buy()
@@ -74,7 +69,8 @@ def get_strategy(name):
 if __name__ =='__main__':
     ticker = 'MARA'
     data = sd.fetch_daily_data(ticker)
-    bt = backtesting.Backtest(data, SMA_10_50_ADX_Strategy, cash=10_000, commission=0.0)
+
+    bt = backtesting.Backtest(data, SMA_10_50_ADX_Strategy, cash=10000)
     bt.run()
     print(bt.stats)
     bt.plot()
