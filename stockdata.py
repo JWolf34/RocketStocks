62dc3df2f922e3394524438f03ab5f66d71eee76
John Wolf <johnmwolf34@gmail.com>
2024-11-20 08:48:31 -0600

    Trying out ratelimti

--- stockdata.py
+++ stockdata.py
@@ -11,10 +11,7 @@ import os
 import datetime
 from datetime import timedelta
 import requests
-from requests import Session
-from requests_cache import CacheMixin, SQLiteCache
-from requests_ratelimiter import LimiterMixin, MemoryQueueBucket
-from pyrate_limiter import Duration, RequestRate, Limiter
+from ratelimit import limits
 import config
 import logging
 from tradingview_screener import Scanner, Query, Column
@@ -22,15 +19,7 @@ from tradingview_screener import Scanner, Query, Column
 # Logging configuration
 logger = logging.getLogger(__name__)
 
-# Class for limiting requests to avoid hitting the rate limit when downloading data
-class CachedLimiterSession(CacheMixin, LimiterMixin, Session):
-    pass
 
-session = CachedLimiterSession(
-    limiter=Limiter(RequestRate(2, Duration.SECOND*5)),  # max 2 requests per 5 seconds
-    bucket_class=MemoryQueueBucket,
-    backend=SQLiteCache("yfinance.cache"),
-) 
 
 class News():
     def __init__(self):
