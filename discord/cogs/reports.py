efc60639896d13d6e0b672be222002e0f9317152
John Wolf <johnmwolf34@gmail.com>
2024-11-22 17:36:21 -0600

    begin saga to properly dockerize the bot

--- discord/cogs/reports.py
+++ discord/cogs/reports.py
@@ -383,7 +383,7 @@ class Reports(commands.Cog):
     # Generate and send premarket gainer reports to the reports channel
     @tasks.loop(minutes=5)
     async def send_gainer_reports(self):
-        logger.info("Posting gainer reports...")
+        print("Posting gainer reports...")
         report = GainerReport(self.gainers_channel)
         if (report.today.weekday() < 5):
             await report.send_report()
@@ -394,6 +394,7 @@ class Reports(commands.Cog):
 
     #@send_gainer_reports.before_loop
     async def before_send_gainer_reports(self):
+        print("got to before loop")
         # Start posting report at next 0 or 5 minute interval
         now = datetime.datetime.now().astimezone()
         if now.minute % 5 == 0:
