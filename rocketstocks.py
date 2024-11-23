5898bc90c3d2f20ce038bedf2e7af1b01aac5a1e
John Wolf <johnmwolf34@gmail.com>
2024-11-23 14:57:52 -0600

    Give it a shot

--- rocketstocks.py
+++ rocketstocks.py
@@ -41,8 +41,7 @@ def setup_logging():
 def rocketStocks():
     logger.info('**********[START LOG]**********')
 
-    Discord.run_bot()
-    """
+
     bot_thread = threading.Thread(target=Discord.run_bot)
     scheduler_thread = threading.Thread(target=scheduler.scheduler)
 
@@ -54,7 +53,7 @@ def rocketStocks():
     
     bot_thread.join()
     logger.debug("Threads joined")
-    """
+    
 
 if (__name__ == '__main__'):
     setup_logging()
