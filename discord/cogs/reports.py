99a9766a1238b5d13c85feb83813917a65885e2e
John Wolf <johnmwolf34@gmail.com>
2024-11-24 22:59:36 -0600

    fixed popularity reports TZ

--- discord/cogs/reports.py
+++ discord/cogs/reports.py
@@ -401,7 +401,7 @@ class Reports(commands.Cog):
     #########
 
     # Report on most popular stocks across reddit daily
-    @tasks.loop(time=datetime.time(hour=18, minute=0, second=0))
+    @tasks.loop(time=datetime.time(hour=18, minute=0, second=0).astimezone())
     async def send_popularity_reports(self):
         report = PopularityReport(self.reports_channel)
         await report.send_report()
