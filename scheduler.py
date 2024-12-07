992cadf86ad633fcd3a5fab8e3ed7c5fb4b40271
John Wolf <johnmwolf34@gmail.com>
2024-12-06 23:23:06 -0600

    5m job time is longer now

--- scheduler.py
+++ scheduler.py
@@ -45,7 +45,7 @@ def scheduler():
     sched.add_job(lambda: sd.StockData.update_daily_price_history(only_today=True), trigger=update_daily_data_daily_trigger, name = "Update daily price history (daily)", timezone=timezone, replace_existing=True)
 
     # Update fiveminutepricehistorytable with recent market data (daily job)
-    # Estimated runtime ~30 seconds, but scales with the number of tickers on watchlists
+    # Estimated runtime ~15 minutes
     sched.add_job(lambda: sd.StockData.update_5m_price_history(only_last_hour=True), trigger= update_5m_data_daily_trigger, name = "Update 5m price history (daily)", timezone=timezone, replace_existing=True)
 
     # Update dailypricehistory table with today's market data (weekly job)
