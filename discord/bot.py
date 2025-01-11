84a0244792442151b02f8697941a867d18c2ca8a
John Wolf <johnmwolf34@gmail.com>
2025-01-10 22:12:17 -0600

    discord logs now write to file

--- discord/bot.py
+++ discord/bot.py
@@ -11,16 +11,24 @@ import logging
 # Logging configuration
 logger = logging.getLogger(__name__)
 
+discord_logger = logging.getLogger('discord')
+for handler in logger.handlers:
+    if handler.name == 'file':
+        discord_logger.addHandler(handler)
+
+# Bot setup
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='$', intents=intents)
 token = config.secrets.discord_token
 
+# Load cogs
 async def load():
     for filename in os.listdir("./discord/cogs"):
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
     logger.info("Loaded extensions")
 
+# Start bot
 def run_bot():
     @bot.event
     async def on_ready():
