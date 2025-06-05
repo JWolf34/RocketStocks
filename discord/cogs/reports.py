5ba64d7e5594abf3a6109547c03acc91eba43d05
BuildTools <unconfigured@null.spigotmc.org>
2025-06-05 00:31:32 -0500

    fixed a few more issues with reports

--- discord/cogs/reports.py
+++ discord/cogs/reports.py
@@ -177,25 +177,28 @@ class Reports(commands.Cog):
             await report.send_report()
 
 
-    @tasks.loop(time=datetime.time(hour=12, minute=0, second=0)) # time in UTC
-    #@tasks.loop(minutes=5)
+    #@tasks.loop(time=datetime.time(hour=12, minute=0, second=0)) # time in UTC
+    @tasks.loop(minutes=5)
     async def post_weekly_earnings(self):
         today = datetime.datetime.now(tz=date_utils.timezone()).date()
-        if today.weekday() == 0:
+        if True: #today.weekday() == 0:
             upcoming_earnings = self.stock_data.earnings.fetch_upcoming_earnings()
+            watchlist_tickers = self.stock_data.watchlists.get_tickers_from_all_watchlists(no_personal=True,
+                                                                                           no_systemGenerated=True)
             report = WeeklyEarningsScreener(channel=self.screeners_channel,
-                                            upcoming_earnings=upcoming_earnings)
+                                            upcoming_earnings=upcoming_earnings,
+                                            watchlist_tickers=watchlist_tickers)
             logger.info(f"Posting weekly earnings report")
             await report.send_report()
 
 
     # Create earnings events on calendar for all stocks on watchlists
-    #@tasks.loop(time=datetime.time(hour=6, minute=0, second=0)) # time in UTC
-    @tasks.loop(minutes=5)
+    @tasks.loop(time=datetime.time(hour=6, minute=0, second=0)) # time in UTC
+    #@tasks.loop(minutes=5)
     async def update_earnings_calendar(self):
         logger.info("Creating calendar events for upcoming earnings dates")
         guild = self.bot.get_guild(utils.discord_utils.guild_id) # Update calendar in target guild
-        tickers = self.stock_data.watchlists.get_tickers_from_all_watchlists(no_personal=False, no_systemGenerated=False) # Get all tickers except from system-generated watchlists
+        tickers = self.stock_data.watchlists.get_tickers_from_all_watchlists(no_personal=False, no_systemGenerated=True) # Get all tickers except from system-generated watchlists
         logger.debug(f"Identified {len(tickers)} watchlist tickers to create earnings events for")
 
         curr_events = await guild.fetch_scheduled_events()
@@ -259,7 +262,7 @@ class Reports(commands.Cog):
 
     @app_commands.command(name = "report-watchlist", description= "Post analysis of a given watchlist (use /fetch-reports for individual or non-watchlist stocks)",)
     @app_commands.describe(watchlist = "Which watchlist to fetch reports for")
-    @app_commands.autocomplete(watchlist=cog_Watchlists.watchlist_options,)
+    @app_commands.autocomplete(watchlist=cog_Watchlists.watchlist_options)
     @app_commands.describe(visibility = "'private' to send to DMs, 'public' to send to the channel")
     @app_commands.choices(visibility =[
         app_commands.Choice(name = "private", value = 'private'),
@@ -267,8 +270,7 @@ class Reports(commands.Cog):
     ])
     async def report_watchlist(self, interaction: discord.Interaction, watchlist: str, visibility: app_commands.Choice[str]):
         await interaction.response.defer(ephemeral=True)
-        logger.info("/report-watchlist function called by user {}".format(interaction.user.name))
-        
+        logger.info(f"/report-watchlist function called by user '{interaction.user.name}'")
         
         message = ""
         watchlist_id = watchlist
@@ -277,17 +279,18 @@ class Reports(commands.Cog):
         if watchlist == 'personal':
             watchlist_id = str(interaction.user.id)
         
-        if watchlist_id not in self.bot.stock_data.watchlists.get_watchlists():
+        if watchlist_id not in self.stock_data.watchlists.get_watchlists():
             await interaction.followup.send(f"Watchlist '{watchlist_id}' does not exist")
-            logger.debug(f"Watchlist '{watchlist}' does not exist. No reports generated. ")
+            logger.info(f"Watchlist '{watchlist}' does not exist. No reports generated. ")
             return
-
-        tickers = self.bot.stock_data.watchlists.get_tickers_from_watchlist(watchlist_id)
+        
+        # Identify tickers in watchlist and generate reports
+        tickers = self.stock_data.watchlists.get_tickers_from_watchlist(watchlist_id)
         logger.info(f"Reports requested for watchlist '{watchlist}' with tickers {tickers}")
 
-        if len(tickers) == 0:
+        if not tickers:
             # Empty watchlist
-            logger.debug("Selected watchlist '{}' is empty".format(watchlist))
+            logger.info(f"Selected watchlist '{watchlist}' is empty")
             message = "No tickers on the watchlist. Use /addticker to build a watchlist."
             await interaction.followup.send(message, ephemeral=True)
         else:
@@ -297,6 +300,7 @@ class Reports(commands.Cog):
             for ticker in tickers:
                 report = await self.build_stock_report(ticker=ticker)
                 message = await report.send_report(interaction, visibility.value)
+             
             logger.info("Reports have been posted")
 
             # Follow-up message
@@ -311,13 +315,14 @@ class Reports(commands.Cog):
         app_commands.Choice(name = "private", value = 'private'),
         app_commands.Choice(name = "public", value = 'public')
     ])        
-    async def report(self, interaction: discord.interactions, tickers: str, visibility: app_commands.Choice[str]):
+    async def report(self, interaction:discord.interactions, tickers: str, visibility: app_commands.Choice[str]):
         await interaction.response.defer(ephemeral=True)
-        logger.info("/report function called by user {}".format(interaction.user.name))
-        logger.info(f"Reports requested for tickers '{tickers}'")
+        logger.info(f"/report function called by user {interaction.user.name}")
+        
     
-        # Validate each ticker in the list is valid
+        # Validate each ticker in the list
         tickers, invalid_tickers = await self.stock_data.parse_valid_tickers(tickers.upper())
+        logger.info(f"Reports requested for tickers {tickers}. Invalid tickers: {invalid_tickers}")
         message = None
         for ticker in tickers:
             report = await self.build_stock_report(ticker=ticker)
@@ -328,7 +333,7 @@ class Reports(commands.Cog):
         if message is not None: # Message was generated
             logger.info(f"Reports posted for tickers {tickers}")
             follow_up = f"Posted reports for tickers [{", ".join(tickers)}]({message.jump_url})!"
-            if len(invalid_tickers) > 0: # More than one invalid ticke input
+            if len(invalid_tickers) > 0: # More than one invalid ticker input
                 follow_up += f" Invalid tickers: {", ".join(invalid_tickers)}"
         if len(tickers) == 0: # No valid tickers input
             logger.info(f"No valid tickers input. No reports generated")
@@ -361,7 +366,6 @@ class Reports(commands.Cog):
         return report
 
     # Autocomplete functions
-
     async def autocomplete_searchin(self, interaction:discord.Interaction, current:str):
         return [
             app_commands.Choice(name = search_in_name, value= search_in_value)
@@ -403,10 +407,12 @@ class Reports(commands.Cog):
         await interaction.response.defer(ephemeral=True)
         logger.info("/popular-stocks function called by user {}".format(interaction.user.name))
         logger.info(f"Latest popularity request from source '{source}'")
+
+        # Get popular stocks df and filter from input source
         popular_stocks = self.stock_data.popularity.get_popular_stocks(filter_name=source)
         filter = self.stock_data.popularity.get_filter(source)
 
-        # Validate popular_stocks
+        # Validate popular_stocks and send report
         if not popular_stocks.empty:
             report = PopularityReport(channel=self.reports_channel, 
                                       popular_stocks=popular_stocks,
@@ -418,6 +424,7 @@ class Reports(commands.Cog):
             await interaction.followup.send(follow_up, ephemeral=True)
             logger.info(f"Popularity posted from source {source}")
         else:
+            # No popular stocks found
             logger.info(f"No popular stocks found with filter '{source}'")
             await interaction.followup.send(f"No popular stocks found with filter '{source}'", ephemeral=True)
 
@@ -430,7 +437,7 @@ class Report(object):
        
         self.channel = channel
 
-        # Parse data from keywords args
+        # Parse data from keyword args
         self.ticker_info = kwargs.pop('ticker_info', None)
         self.ticker = self.ticker_info['ticker'] if self.ticker_info else kwargs.pop('ticker', None)
         self.quote = kwargs.pop('quote', None)
@@ -1380,10 +1387,10 @@ class EarningsSpotlightReport(Report):
                 await interaction.response.send_message(f"Fetched news for {self.ticker}!", ephemeral=True)
 
 class WeeklyEarningsScreener(Screener):
-    def __init__(self, channel:discord.channel, upcoming_earnings:pd.DataFrame):
+    def __init__(self, channel:discord.channel, upcoming_earnings:pd.DataFrame, watchlist_tickers:list):
 
         self.today = datetime.datetime.now(tz=date_utils.timezone()).date()
-
+        self.watchlist_tickers = watchlist_tickers
         self.upcoming_earnings = upcoming_earnings[upcoming_earnings['date'].between(self.today, self.today + datetime.timedelta(days=7))]
         column_map = {'date': 'Date',
                       'ticker': 'Ticker',
@@ -1416,7 +1423,7 @@ class WeeklyEarningsScreener(Screener):
             date = self.today + datetime.timedelta(days=i)
             tickers = self.data[self.data['Date'] == date]['Ticker'].values
             if tickers.any(): # np array
-                watchlist_earnings[date.strftime('%A')] = tickers
+                watchlist_earnings[date.strftime('%A')] = [ticker for ticker in tickers if ticker in self.watchlist_tickers]
 
         watchlist_earnings_df = pd.DataFrame(dict([(date, pd.Series(tickers)) for date, tickers in watchlist_earnings.items()])).fillna(' ')
         message = self.build_build_df_table(df=watchlist_earnings_df, style='borderless')
