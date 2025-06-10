9a2787bc2d6f04528f72a783b7af08b43891d4e6
BuildTools <unconfigured@null.spigotmc.org>
2025-06-10 13:45:18 -0500

    more docs improvement for utils

--- discord/cogs/utils.py
+++ discord/cogs/utils.py
@@ -16,8 +16,9 @@ class Utils(commands.Cog):
         logger.info(f"Cog {__name__} loaded!")
 
     @app_commands.command(name='sync', description='Sync bot commands to the server')
-    @app_commands.checks.has_permissions(administrator=True)
+    @app_commands.checks.has_permissions(owner=True)
     async def sync(self, interaction:discord.Interaction):
+        """Sync bot commands to Discord's servers. Use this after adding or removing an app command"""
         logger.info("/sync command called by user {}".format(interaction.user.name))
         await self.bot.tree.sync()
         await interaction.response.send_message("Bot commands synced!", ephemeral=True)
@@ -25,17 +26,21 @@ class Utils(commands.Cog):
 
     @app_commands.command(name = "help", description= "Show help on the bot's commands",)
     async def help(self, interaction: discord.Interaction):
+        """Post message in discord with documentation on the bot's commands"""
         logger.info("/help function called by user {}".format(interaction.user.name))
         embeds = []
+
+        # Iterate over each cog and generate embed with cog's commands
         for cog_name, cog in self.bot.cogs.items():
             commands = cog.get_app_commands()
             if commands:
                 embed = discord.Embed()
                 embed.title = cog_name
                 for command in commands:
-                    if not command.checks:
+                    # Only show commands that the user meets checks for
+                    if all([check(interaction) for check in command.checks]):
                         embed.add_field(name=f"/{command.name}", value=command.description)
-                
+                # Only add embed if at least command is available to that user in a cog
                 if embed.fields:
                     embeds.append(embed)
         await interaction.response.send_message(embeds=embeds)
