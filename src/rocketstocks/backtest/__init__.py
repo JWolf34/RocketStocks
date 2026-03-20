a773c56ffa857a031821861fb3b2265683b2af7b
John Wolf <johnmwol34@gmail.com>
2026-03-20 16:35:41 -0500

    backtest entrypoint

---
+++ src/rocketstocks/backtest/__init__.py
@@ -0,0 +1,1 @@
+"""Backtesting system for RocketStocks signal validation."""
---
+++ src/rocketstocks/backtest/__main__.py
@@ -0,0 +1,15 @@
+"""Entry point: python -m rocketstocks.backtest"""
+import asyncio
+import sys
+
+from rocketstocks.core.utils.logging_config import setup_logging
+from rocketstocks.backtest.cli import main
+
+
+def _entry():
+    setup_logging()
+    sys.exit(asyncio.run(main()))
+
+
+if __name__ == '__main__':
+    _entry()
