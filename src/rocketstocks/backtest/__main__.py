45d48a93ea6873684d521a11ea553522e3e4d6cf
John Wolf <johnmwol34@gmail.com>
2026-03-23 10:26:41 -0500

    info logs during backtests

--- src/rocketstocks/backtest/__main__.py
+++ src/rocketstocks/backtest/__main__.py
@@ -1,13 +1,14 @@
 """Entry point: python -m rocketstocks.backtest"""
 import asyncio
 import sys
+import logging
 
 from rocketstocks.core.utils.logging_config import setup_logging
 from rocketstocks.backtest.cli import main
 
 
 def _entry():
-    setup_logging()
+    setup_logging(level=logging.INFO)
     sys.exit(asyncio.run(main()))
 
 
